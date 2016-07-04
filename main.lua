4be1131593b9dfebabce3aed21fe9a73c6403b38
Dan Cecile <dancecile@gmail.com>
2016-07-04 17:46:29 -0400

    Debug environment variable to avoid mispackaging

--- main.lua
+++ main.lua
@@ -5,17 +5,18 @@ local utils = require('utils')
 
 music = nil
 credits = nil
-local fullscreen = false
 local quitShortcut = false
 
 extraScreens.introScreen.gameScreen = game.screen
 
 currentScreen = extraScreens.titleScreen
-extraScreens.titleScreen.mute = true
---fullscreen = true
-quitShortcut = true
-currentScreen = game.screen
-game.screen:start()
+
+if os.getenv('DEBUG_URBS') then
+  extraScreens.titleScreen.mute = true
+  quitShortcut = true
+  currentScreen = game.screen
+  game.screen:start()
+end
 
 function love.load()
   love.window.setTitle('Ultra Rainbow Bake Sale')
@@ -25,7 +26,7 @@ function love.load()
     {
       resizable = true,
       msaa = 8,
-      fullscreen = fullscreen,
+      fullscreen = false,
     })
   bigFont = love.graphics.newFont(24)
   music = love.audio.newSource('bensound-anewbeginning.mp3')
